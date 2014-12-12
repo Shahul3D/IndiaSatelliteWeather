a0516cd5c616cd53f6686f4edc46900989e67186
Shahul <shahul@stayzilla.com>
2014-12-12 23:48:49 +0530

    Shahul: Included events for map downloading..

--- app/src/main/java/com/shahul3d/indiasatelliteweather/service/DownloaderService.java
+++ app/src/main/java/com/shahul3d/indiasatelliteweather/service/DownloaderService.java
@@ -5,14 +5,19 @@ import android.content.Intent;
 import android.os.IBinder;
 
 import com.noveogroup.android.log.Log;
+import com.shahul3d.indiasatelliteweather.events.DownloadCompletedEvent;
+import com.shahul3d.indiasatelliteweather.events.DownloadRequestEvent;
 import com.shahul3d.indiasatelliteweather.events.TestEvent;
 
+import org.androidannotations.annotations.Background;
 import org.androidannotations.annotations.EService;
 
 import de.greenrobot.event.EventBus;
 
 @EService
 public class DownloaderService extends Service {
+
+    EventBus bus = EventBus.getDefault();
 
     @Override
     public IBinder onBind(Intent intent) {
@@ -22,16 +27,32 @@ public class DownloaderService extends Service {
     @Override
     public void onCreate() {
         super.onCreate();
-        EventBus.getDefault().register(this);
+        bus.register(this);
         Log.a("Service On Create");
     }
 
     @Override
     public void onDestroy() {
-        EventBus.getDefault().unregister(this);
+        bus.unregister(this);
         Log.a("Service On Destroy");
 
         super.onDestroy();
+    }
+
+    public void onEvent(DownloadRequestEvent downloadRequest) {
+        String mapType = downloadRequest.getMapTypeToDownload();
+        downloadMap(mapType);
+    }
+
+    @Background
+    private void downloadMap(String mapType) {
+        //download logic here...
+        publishDownloadComplete(mapType);
+    }
+
+    //Notify the Views to refresh to get the updated map.
+    private void publishDownloadComplete(String mapType) {
+        bus.post(new DownloadCompletedEvent(mapType));
     }
 
     public void onEvent(TestEvent event) {
