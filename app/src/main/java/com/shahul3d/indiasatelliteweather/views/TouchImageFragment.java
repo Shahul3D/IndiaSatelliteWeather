eb1d38d28cd356dfc1edc1b0fb224c769f96f76c
Shahul <shahul@stayzilla.com>
2014-12-18 10:36:54 +0530

    Shahul: Moved event bus from activity to fragments.

--- app/src/main/java/com/shahul3d/indiasatelliteweather/views/TouchImageFragment.java
+++ app/src/main/java/com/shahul3d/indiasatelliteweather/views/TouchImageFragment.java
@@ -1,15 +1,20 @@
 package com.shahul3d.indiasatelliteweather.views;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.MenuItem;
 
 import com.davemorrissey.labs.subscaleview.SubsamplingScaleImageView;
 import com.noveogroup.android.log.Log;
 import com.shahul3d.indiasatelliteweather.R;
+import com.shahul3d.indiasatelliteweather.events.TestEvent;
 
 import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EFragment;
 import org.androidannotations.annotations.FragmentArg;
 import org.androidannotations.annotations.ViewById;
+
+import de.greenrobot.event.EventBus;
 
 @EFragment(R.layout.touch_image_fragment)
 public class TouchImageFragment extends Fragment {
@@ -18,6 +23,26 @@ public class TouchImageFragment extends Fragment {
 
     @ViewById
     SubsamplingScaleImageView touchImage;
+
+    EventBus bus = EventBus.getDefault();
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        bus.register(this);
+    }
+
+    @Override
+    public void onPause() {
+        bus.unregister(this);
+        super.onPause();
+    }
 
     @AfterViews
     void calledAfterViewInjection() {
@@ -37,6 +62,18 @@ public class TouchImageFragment extends Fragment {
         return defaultImage;
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_refresh) {
+            Log.a("Refresh clicked..");
+            bus.post(new TestEvent("Shahul"));
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
 //        touchImage.setImageResource(chooseImage(pageNumber));
 //        touchImage.setImageBitmap(chooseImage(pageNumber));
 //        loadImage(chooseImage(pageNumber));
@@ -54,47 +91,49 @@ public class TouchImageFragment extends Fragment {
 //        touchImage.setImageBitmap(image);
 //    }
 
-/*
+    /*
 
-    public void setImageInViewPager(int itemData) {
-        try {
-            //if image size is too large. Need to scale as below code.
+        public void setImageInViewPager(int itemData) {
+            try {
+                //if image size is too large. Need to scale as below code.
 
-            BitmapFactory.Options options = new BitmapFactory.Options();
-            options.inJustDecodeBounds = true;
-            myBitmap = BitmapFactory.decodeResource(getResources(), itemData,
-                    options);
-            if (options.outWidth > 3000 || options.outHeight > 2000) {
-                options.inSampleSize = 4;
-            } else if (options.outWidth > 2000 || options.outHeight > 1500) {
-                options.inSampleSize = 3;
-            } else if (options.outWidth > 1000 || options.outHeight > 1000) {
-                options.inSampleSize = 2;
-            }
-            options.inJustDecodeBounds = false;
-            myBitmap = BitmapFactory.decodeResource(getResources(), itemData,
-                    options);
-            if (myBitmap != null) {
-                try {
-                    if (touchImage != null) {
-                        touchImage.setImageBitmap(myBitmap);
-                    }
-                } catch (Exception e) {
-                    e.printStackTrace();
+                BitmapFactory.Options options = new BitmapFactory.Options();
+                options.inJustDecodeBounds = true;
+                myBitmap = BitmapFactory.decodeResource(getResources(), itemData,
+                        options);
+                if (options.outWidth > 3000 || options.outHeight > 2000) {
+                    options.inSampleSize = 4;
+                } else if (options.outWidth > 2000 || options.outHeight > 1500) {
+                    options.inSampleSize = 3;
+                } else if (options.outWidth > 1000 || options.outHeight > 1000) {
+                    options.inSampleSize = 2;
                 }
+                options.inJustDecodeBounds = false;
+                myBitmap = BitmapFactory.decodeResource(getResources(), itemData,
+                        options);
+                if (myBitmap != null) {
+                    try {
+                        if (touchImage != null) {
+                            touchImage.setImageBitmap(myBitmap);
+                        }
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            } catch (OutOfMemoryError e) {
+                e.printStackTrace();
+                System.gc();
             }
-        } catch (OutOfMemoryError e) {
-            e.printStackTrace();
-            System.gc();
         }
-    }
 
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        if (myBitmap != null) {
-            myBitmap.recycle();
-            myBitmap = null;
-        }
-    }*/
+        @Override
+        public void onDestroyView() {
+            super.onDestroyView();
+            if (myBitmap != null) {
+                myBitmap.recycle();
+                myBitmap = null;
+            }
+        }*/
+    public void onEvent(Object e) {
+    }
 }
