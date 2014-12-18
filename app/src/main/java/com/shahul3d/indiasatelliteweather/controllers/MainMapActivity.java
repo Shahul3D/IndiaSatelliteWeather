ebc63dfade4c559d4aea93ae4354042d41cd2c6f
Shahul3D <shahul3d@gmail.com>
2014-12-18 10:36:54 +0530

    Shahul: Moved event bus from activity to fragments.

--- app/src/main/java/com/shahul3d/indiasatelliteweather/controllers/MainMapActivity.java
+++ app/src/main/java/com/shahul3d/indiasatelliteweather/controllers/MainMapActivity.java
@@ -9,14 +9,12 @@ import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import com.noveogroup.android.log.Log;
 import com.shahul3d.indiasatelliteweather.R;
 import com.shahul3d.indiasatelliteweather.adapters.TouchImagePageAdapter;
-import com.shahul3d.indiasatelliteweather.events.TestEvent;
 import com.shahul3d.indiasatelliteweather.service.DownloaderService_;
 import com.shahul3d.indiasatelliteweather.utils.StorageUtils;
 import com.shahul3d.indiasatelliteweather.widgets.SlidingTabLayout;
@@ -25,8 +23,6 @@ import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.Bean;
 import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.ViewById;
-
-import de.greenrobot.event.EventBus;
 
 @EActivity(R.layout.activity_main_map)
 public class MainMapActivity extends ActionBarActivity {
@@ -51,19 +47,16 @@ public class MainMapActivity extends ActionBarActivity {
     @Bean
     StorageUtils storageUtils;
 
-    EventBus bus = EventBus.getDefault();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        bus.register(this);
         //Starting the service when the app starts
         DownloaderService_.intent(getApplication()).start();
     }
 
     @Override
     protected void onDestroy() {
-        bus.unregister(this);
         //Stopping the service when the app exists.
         DownloaderService_.intent(getApplication()).stop();
         super.onDestroy();
@@ -75,10 +68,6 @@ public class MainMapActivity extends ActionBarActivity {
         initToolbar();
         initDrawer();
         Log.a("Storage path: %s", storageUtils.getExternalStoragePath());
-    }
-
-    public void onEvent(TestEvent event) {
-        System.out.println("Activity  got the message: " + event.getData());
     }
 
     private void initDrawer() {
@@ -129,20 +118,19 @@ public class MainMapActivity extends ActionBarActivity {
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            Log.a("Settings clicked..");
-            bus.post(new TestEvent("Shahul"));
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
+//        int id = item.getItemId();
+//
+//        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_refresh) {
+//            Log.a("refresh clicked..");
+//            return true;
+//        }
+//
+//        return super.onOptionsItemSelected(item);
+//    }
 }
