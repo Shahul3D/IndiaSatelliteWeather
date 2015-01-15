e26f30871c5f4449d035ffbefc59f76539e2a7ac
Shahul3D <shahul3d@gmail.com>
2015-01-16 03:32:54 +0530

    Shahul: Included copyright details.

--- app/src/main/java/com/shahul3d/indiasatelliteweather/controllers/AnimatedMapActivity.java
+++ app/src/main/java/com/shahul3d/indiasatelliteweather/controllers/AnimatedMapActivity.java
@@ -1,3 +1,18 @@
+/*
+ * ******************************************************************************
+ *  * Copyright (c) 2015.  Shahul Hameed.
+ *  *
+ *  * Licensed under GNU GENERAL PUBLIC LICENSE;
+ *  * you may not use this file except in compliance with the License.
+ *  *
+ *  * Unless required by applicable law or agreed to in writing, software
+ *  * distributed under the License is distributed on an "AS IS" BASIS,
+ *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  * See the License for the specific language governing permissions and
+ *  * limitations under the License.
+ *  ******************************************************************************
+ */
+
 package com.shahul3d.indiasatelliteweather.controllers;
 
 import android.content.res.Configuration;
