720bfdca9848c29fa05475afc8eeca245e87a669
Shahul3D <shahul3d@gmail.com>
2015-02-27 23:30:54 +0530

    Shahul: Included utility file to get formatted updated time of the map.

---
+++ app/src/main/java/com/shahul3d/indiasatelliteweather/utils/FormatUtils.java
@@ -0,0 +1,55 @@
+/*
+ * ******************************************************************************
+ *  * Copyright (c) 2015.  Shahul Hameed.
+ *  *
+ *  * Licensed under GNU GENERAL PUBLIC LICENSE;
+ *  * you may not use this file except in compliance with the License.
+ *  *
+ *  * Unless required by applicable law or agreed to in writing, software
+ *  * distributed under the License is distributed on an "AS IS" BASIS,
+ *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  * See the License for the specific language governing permissions and
+ *  * limitations under the License.
+ *  ******************************************************************************
+ */
+
+package com.shahul3d.indiasatelliteweather.utils;
+
+import org.androidannotations.annotations.EBean;
+
+@EBean
+public class FormatUtils {
+    public static String getTimeAgo(long time) {
+        final int SECOND_MILLIS = 1000;
+        final int MINUTE_MILLIS = 60 * SECOND_MILLIS;
+        final int HOUR_MILLIS = 60 * MINUTE_MILLIS;
+        final int DAY_MILLIS = 24 * HOUR_MILLIS;
+
+        if (time < 1000000000000L) {
+            // if timestamp given in seconds, convert to millis
+            time *= 1000;
+        }
+
+        long now = System.currentTimeMillis();
+        if (time > now || time <= 0) {
+            return null;
+        }
+
+        final long diff = now - time;
+        if (diff < MINUTE_MILLIS) {
+            return "just now";
+        } else if (diff < 2 * MINUTE_MILLIS) {
+            return "a minute ago";
+        } else if (diff < 50 * MINUTE_MILLIS) {
+            return diff / MINUTE_MILLIS + " minutes ago";
+        } else if (diff < 90 * MINUTE_MILLIS) {
+            return "an hour ago";
+        } else if (diff < 24 * HOUR_MILLIS) {
+            return diff / HOUR_MILLIS + " hours ago";
+        } else if (diff < 48 * HOUR_MILLIS) {
+            return "yesterday";
+        } else {
+            return diff / DAY_MILLIS + " days ago";
+        }
+    }
+}
