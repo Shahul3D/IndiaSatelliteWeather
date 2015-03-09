cb81f7ee44ca0324bc09cc24e74a28640fce571b
Shahul3D <shahul3d@gmail.com>
2015-03-09 21:03:35 +0530

    Shahul: Removed unused method.

--- app/src/main/java/com/shahul3d/indiasatelliteweather/utils/StorageUtils.java
+++ app/src/main/java/com/shahul3d/indiasatelliteweather/utils/StorageUtils.java
@@ -1,25 +1,11 @@
 package com.shahul3d.indiasatelliteweather.utils;
 
-import android.content.Context;
-import android.support.v4.content.ContextCompat;
-
 import org.androidannotations.annotations.EBean;
-import org.androidannotations.annotations.RootContext;
 
 import java.io.File;
 
 @EBean
 public class StorageUtils {
-
-    @RootContext
-    Context context;
-
-    public String getExternalStoragePath() {
-
-        File[] dir = ContextCompat.getExternalFilesDirs(context, null);
-        String location = dir[dir.length - 1].toString();
-        return location ;
-    }
     public boolean fileExists(String filename) {
         File file = new File(filename);
         if(file == null || !file.exists()) {
