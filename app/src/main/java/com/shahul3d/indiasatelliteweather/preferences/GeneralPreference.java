ddce621e04d35c5e93dfaee5e800fc58fa72cce1
Shahul3D <shahul3d@gmail.com>
2015-06-12 02:58:19 +0530

    Shahul: Added preferences screen for auto update maps.

---
+++ app/src/main/java/com/shahul3d/indiasatelliteweather/preferences/GeneralPreference.java
@@ -0,0 +1,73 @@
+/*
+ *
+ *  * ******************************************************************************
+ *  *  * Copyright (c) 2015.  Shahul Hameed.
+ *  *  *
+ *  *  * Licensed under GNU GENERAL PUBLIC LICENSE;
+ *  *  * you may not use this file except in compliance with the License.
+ *  *  *
+ *  *  * Unless required by applicable law or agreed to in writing, software
+ *  *  * distributed under the License is distributed on an "AS IS" BASIS,
+ *  *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  *  * See the License for the specific language governing permissions and
+ *  *  * limitations under the License.
+ *  *  ******************************************************************************
+ *
+ */
+
+package com.shahul3d.indiasatelliteweather.preferences;
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceFragment;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+import com.shahul3d.indiasatelliteweather.R;
+
+import java.util.List;
+
+public class GeneralPreference extends PreferenceActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        prepareLayout();
+    }
+
+    @Override
+    public void onBuildHeaders(List<Header> target) {
+        loadHeadersFromResource(R.xml.pref_headers, target);
+    }
+
+    private void prepareLayout() {
+        ViewGroup root = (ViewGroup) findViewById(android.R.id.content);
+        View content = root.getChildAt(0);
+        LinearLayout toolbarContainer = (LinearLayout) View.inflate(this, R.layout.activity_prefs, null);
+
+        root.removeAllViews();
+        toolbarContainer.addView(content);
+        root.addView(toolbarContainer);
+
+        Toolbar mToolBar = (Toolbar) toolbarContainer.findViewById(R.id.toolbar);
+        mToolBar.setTitle(getTitle());
+        mToolBar.setNavigationIcon(R.drawable.abc_ic_ab_back_mtrl_am_alpha);
+        mToolBar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+
+    public static class MyPreferenceFragment extends PreferenceFragment
+    {
+        @Override
+        public void onCreate(final Bundle savedInstanceState)
+        {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.preferences);
+        }
+    }
+}
